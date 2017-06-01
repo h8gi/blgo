b170d217bb476e8fb720164d7f361ce0bc6b9006
hiroki yagi <hiro.yagi0245@gmail.com>
2017-06-01 11:53:13 +0900

    fix update method

--- controllers/fragments_controller.go
+++ controllers/fragments_controller.go
@@ -15,9 +15,9 @@ func GetFragmentsList(c echo.Context) error {
 
 // TODO: Check form value. Don't use `c.bind`.
 func PostFragment(c echo.Context) error {
-	f := new(models.Fragment)
-	if err := c.Bind(f); err != nil {
-		return err
+	text := c.FormValue("text")
+	f := &models.Fragment{
+		Text: text,
 	}
 	if err := DB.Create(f).Error; err != nil {
 		return err
@@ -29,17 +29,19 @@ func GetFragment(c echo.Context) error {
 	id := c.Param("id")
 	f := new(models.Fragment)
 	if DB.First(f, "id = ?", id).RecordNotFound() {
-		return c.JSON(http.StatusNotFound, map[string]string{"message": "Not Found"})
+		return echo.NewHTTPError(http.StatusNotFound)
 	}
 	return c.JSON(http.StatusOK, f)
 }
 
 func UpdateFragment(c echo.Context) error {
+	id := c.Param("id")
+	text := c.Param("text")
 	f := new(models.Fragment)
-	if err := c.Bind(f); err != nil {
-		return err
+	if DB.First(f, "id = ?", id).RecordNotFound() {
+		return echo.NewHTTPError(http.StatusNotFound)
 	}
-	if err := DB.Update(f).Error; err != nil {
+	if err := DB.Model(f).Update("text", text).Error; err != nil {
 		return err
 	}
 	return c.JSON(http.StatusOK, f)
@@ -49,7 +51,7 @@ func DeleteFragment(c echo.Context) error {
 	id := c.Param("id")
 	f := new(models.Fragment)
 	if DB.First(f, "id = ?", id).RecordNotFound() {
-		return c.JSON(http.StatusNotFound, map[string]string{"message": "Not Found"})
+		return echo.NewHTTPError(http.StatusNotFound)
 	}
 	if err := DB.Delete(f).Error; err != nil {
 		return err
